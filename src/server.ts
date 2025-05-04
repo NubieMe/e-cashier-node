37470fa71dc52b813cf55ae9bf49aa378444a61b
fahmi <fahmi.mellow@gmail.com>
2025-05-04 15:51:20 +0700

    feat: make uploads folder public

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ export const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
+app.use(express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
