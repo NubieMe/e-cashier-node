dafe11d15cfb2c41e271da70870f4a75bbe5741d
fahmi <fahmi.mellow@gmail.com>
2025-05-04 15:50:32 +0700

    chore: register attendance, master & inventory router

--- src/route.ts
+++ src/route.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import RoleRoute from './module/master/role/route';
 import UserRoute from './module/master/user/route';
+import AttendanceRoute from './module/attendance/route';
+import MasterRoute from './module/master/route';
+import InventoryRoute from './module/inventory/route';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-router.use("/role", RoleRoute);
 router.use("/user", UserRoute);
+router.use("/attendance", AttendanceRoute);
+router.use("/master", MasterRoute);
+router.use("/inventory", InventoryRoute);
 
 export default router;
