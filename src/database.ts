9d73bde4702f215a3e0eb9856aec0f4c8f0cd6c8
fahmi <fahmi.mellow@gmail.com>
2025-05-04 15:37:23 +0700

    chore: set timeout & min pool size for db

--- src/database.ts
+++ src/database.ts
@@ -4,9 +4,10 @@ import "dotenv/config"
 const address = process.env.DATABASE_URL!;
 
 export function connect() {
-    const db = mongoose.connection;
-
-    mongoose.connect(address);
+    const db = mongoose.createConnection(address, {
+        socketTimeoutMS: 10000,
+        minPoolSize: 10,
+    });
 
     db.on("error", (error) => console.error(error));
     db.once("open", async () => {
